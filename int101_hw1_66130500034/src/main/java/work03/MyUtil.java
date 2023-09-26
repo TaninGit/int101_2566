6402b54fa4e9abd4fd6b73570f86cc4c68cd0f76
TaninGit <tanin.kaoa@mail.kmutt.ac.th>
2023-09-26 21:57:47 +0700

    first commit

---
+++ int101_hw1_66130500034/src/main/java/work03/MyUtil.java
@@ -0,0 +1,13 @@
+
+package work03;
+
+public final class MyUtil {
+    public static double calculateBMI (double weight, double height){
+        return weight / height * height;
+    }
+    
+    public static int average (int v1, int v2, int v3){
+        return (v1+v2+v3)/3;
+    }
+
+}
